cce315618ee741bd05438603996423338360bed7
tranducduy2002 <duy.tranduc2002@gmail.com>
2022-06-12 22:47:08 +0800

    dropdown

--- src/Code_structure/BasicTemplate.java
+++ src/Code_structure/BasicTemplate.java
@@ -4,8 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,8 +16,8 @@ public class BasicTemplate {
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
---
+++ src/ExerciseDropDown/Exercise_Dropdown_02.java
@@ -0,0 +1,59 @@
+package ExerciseDropDown;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Exercise_Dropdown_02 {
+	WebDriver driver;
+	Select select;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void TC_01_NopCommerce() {
+		driver.get("https://demo.nopcommerce.com/register");
+		
+		driver.findElement(By.id("FirstName")).sendKeys();
+		driver.findElement(By.id("LastName")).sendKeys();
+	
+		// Dropdown
+		// Khi nào dùng để thao tác với element thì mới khởi tạo
+		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		
+		// thao tác với dropdown
+		
+		// Index: là thứ tự của thẻ option
+		select.selectByIndex(1);
+		select.selectByIndex(5);
+		
+		// Value: là giá trị của thuộc tính "value"
+		select.selectByValue("1");
+		select.selectByValue("5");
+		
+		// Text: là item text
+		select.selectByVisibleText("Day");
+		select.selectByVisibleText("5");
+
+	}
+
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
