59d98f3aa58a2592f4efcb461ba213cbf695d6b4
Duy Tran <duy.tranduc2002@gmail.com>
2022-06-21 22:42:17 +0800

    checkbox

--- src/Button_Alert/Button_Checkbox.java
+++ src/Button_Alert/Button_Checkbox.java
@@ -1,9 +1,12 @@
 package Button_Alert;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.Color;
 import org.testng.Assert;
@@ -28,7 +31,7 @@ public class Button_Checkbox {
       
 
 	@Test
-	public void TC_01_EditableDropdown() {
+	public void TC_01_Button() {
 		driver.get("https://www.fahasa.com/customer/account/create");
 		// Xử lý popup thì không cần sleep cứng
 		sleepInSecond(4);
@@ -52,7 +55,79 @@ public class Button_Checkbox {
 	
 	}
 
+	@Test
+	public void TC_02_Checbox() {
+		driver.get("https://automationfc.github.io/multiple-fields/");
+		
+		// 1- Chọn (Choice) - Click
+		driver.findElement(By.cssSelector("input[value='Emotional Disorder']")).click();
+		driver.findElement(By.cssSelector("input[value='Digestive Problems']")).click();
+		driver.findElement(By.cssSelector("input[value='Venereal Disease']")).click();
+		sleepInSecond(3);
+		
+		driver.findElement(By.cssSelector("input[value='5+ days']")).click();
+		driver.findElement(By.cssSelector("input[value=\"I don't have a diet plan\"]")).click();
+		driver.findElement(By.cssSelector("input[value=\"3-4 glasses/day\"]")).click();
+		sleepInSecond(3);
+		
+		// 2 - Verify (Chọn rồi hay chưa chọn thành công)
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='Emotional Disorder']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='Digestive Problems']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='Venereal Disease']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='5+ days']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value=\"I don't have a diet plan\"]")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='3-4 glasses/day']")).isSelected());
+		
+		// 3 - Bỏ chọn (De-select)
+		driver.findElement(By.cssSelector("input[value='Emotional Disorder']")).click();
+		driver.findElement(By.cssSelector("input[value='Digestive Problems']")).click();
+		driver.findElement(By.cssSelector("input[value='Venereal Disease']")).click();
+		sleepInSecond(3);
+		
+		driver.findElement(By.cssSelector("input[value='5+ days']"));
+		driver.findElement(By.cssSelector("input[value=\"I don't have a diet plan\"]"));
+		driver.findElement(By.cssSelector("input[value='3-4 glasses/day']"));
+		sleepInSecond(3);
+		
+		// 4 - 
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[value='Emotional Disorder']")).isSelected());
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[value='Digestive Problems']")).isSelected());
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[value='Venereal Disease']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='5+ days']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value=\"I don't have a diet plan\"]")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[value='3-4 glasses/day']")).isSelected());
+		
+	
+	}
+	
+	@Test
+	public void TC_03_Select_All_Checkbox() {
+		driver.get("https://automationfc.github.io/multiple-fields/");
+		
+		List<WebElement> allCheckboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+		
+		for (WebElement checkbox : allCheckboxes) {
+			if (!checkbox.isSelected()) {
+			checkbox.click();
+			sleepInSecond(1);
+			}
+			Assert.assertTrue(checkbox.isSelected()); // Verify
 
+		}
+		
+		// Verify all checkboxes are deselected
+		for (WebElement checkbox : allCheckboxes) {
+			if (checkbox.isSelected()) {
+				checkbox.click();
+				sleepInSecond(1);
+			}
+			Assert.assertFalse(checkbox.isSelected());
+
+		}
+		
+		
+	}
+	
 	public void sleepInSecond(long timeInSecond) {
 	try {
 		Thread.sleep(timeInSecond * 1000);
---
+++ src/Button_Alert/Exercise_Checkbox.java
@@ -0,0 +1,73 @@
+package Button_Alert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Exercise_Checkbox {
+
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	Select select;
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	
+	}
+
+	@Test
+	public void TC_01_DefaultCheckBoxandRadioButton() {
+		driver.get("https://demos.telerik.com/kendo-ui/checkbox/index");
+		List<WebElement> allCheckboxes = driver.findElements(By.cssSelector("input.k-checkbox"));
+		
+		for (WebElement checkbox : allCheckboxes) {
+			if (checkbox.isEnabled()) {
+				if (!checkbox.isSelected()) {
+					checkbox.click();
+					sleepInMiliSecond(300);
+										
+				}
+				Assert.assertTrue(checkbox.isSelected());
+			}
+			
+		}
+		
+		driver.get("https://demos.telerik.com/kendo-ui/radiobutton/index");
+		
+		WebElement enginethree = driver.findElement(By.cssSelector("input#engine3"));
+		if (!enginethree.isSelected()) {
+			enginethree.click();
+			sleepInMiliSecond(500);
+		}
+		Assert.assertTrue(enginethree.isSelected());
+	
+	}
+
+
+	public void sleepInMiliSecond(long timeInMiliSecond) {
+	try {
+		Thread.sleep(timeInMiliSecond);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+	}
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+}
