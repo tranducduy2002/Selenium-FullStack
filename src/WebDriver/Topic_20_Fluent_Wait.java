dbdb0a938f30e563b1ce26842a623ed8ee9e9c34
Duy Tran <duy.tranduc2002@gmail.com>
2022-08-06 15:38:17 +0800

    FluentWait

--- src/WebDriver/Topic_20_Fluent_Wait.java
+++ src/WebDriver/Topic_20_Fluent_Wait.java
@@ -9,12 +9,16 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import com.google.common.base.Function;
+
+import net.bytebuddy.asm.Advice.Local;
 
 public class Topic_20_Fluent_Wait {
 	WebDriver driver;
@@ -23,7 +27,8 @@ public class Topic_20_Fluent_Wait {
 	FluentWait<WebDriver> fluentDriver;
 	
 	FluentWait<WebElement> fluentElement;
-	 
+	private long sumTime = 30;
+	private long pollTime = 1;
 	
 	
 	@BeforeClass
@@ -85,9 +90,70 @@ public class Topic_20_Fluent_Wait {
 		Assert.assertTrue(loginButtonStatus);
 	}
 	
+	@Test
 	public void TC_01_Fluent_Wait_Excersise() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		fluentDriver = new FluentWait<WebDriver>(driver);
 		
+		driver.findElement(By.cssSelector("div#start button")).click();
 		
+		fluentDriver.withTimeout(Duration.ofSeconds(15))
+		.pollingEvery(Duration.ofSeconds(1))
+		.ignoring(NoSuchElementException.class)
+		.until(new Function<WebDriver, Boolean>(){
+		public Boolean apply(WebDriver driver) {
+			boolean textStatus = driver.findElement(By.cssSelector("div#finish h4")).isDisplayed();
+			System.out.println(textStatus);
+			return textStatus;					
+		}
+		});
+	}
+	
+	
+	@Test
+	public void TC_02_Fluent_Wait_Excersise_WithFunction() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		fluentDriver = new FluentWait<WebDriver>(driver);
+		
+		clickToElement(By.xpath("//div[@id='start']/button"));
+		isElementDisplayed(By.xpath("//div[@id='finish']/h4"));
+		
+	}
+	public WebElement findElement(By locator) {
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(sumTime)).pollingEvery(Duration.ofSeconds(pollTime))
+				.ignoring(NoSuchElementException.class);
+		
+		WebElement element = wait.until(new Function<WebDriver, WebElement>(){
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(locator);
+			}
+		});
+		return element;
+	}
+	
+	public void clickToElement(By locator) {
+		WebElement element = findElement(locator);
+		element.click();
+	}
+	
+	public WebElement waitElementVisible(By locator) {
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(sumTime)).pollingEvery(Duration.ofSeconds(pollTime))
+				.ignoring(NoSuchElementException.class);
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+								
+	}
+	
+	public boolean isElementDisplayed(By locator) {
+		WebElement element = waitElementVisible(locator);
+		FluentWait<WebElement> wait = new FluentWait<WebElement>(element).withTimeout(Duration.ofSeconds(sumTime)).pollingEvery(Duration.ofSeconds(pollTime))
+				.ignoring(NoSuchElementException.class);
+		boolean isDisplayed = wait.until(new Function<WebElement, Boolean>(){
+			public Boolean apply(WebElement element) {
+				boolean flag = element.isDisplayed();
+				return flag;
+			}
+		});
+		return isDisplayed;
 	}
 	
 	public String getCurrentTime() {
