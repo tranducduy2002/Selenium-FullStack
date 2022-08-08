1257f0d08d1fae20918661348d9ef72cc3ab6919
Duy Tran <duy.tranduc2002@gmail.com>
2022-08-09 00:30:57 +0800

    forExercise

--- src/JavaBasic/Topic_08_For_Exercise.java
+++ src/JavaBasic/Topic_08_For_Exercise.java
@@ -3,6 +3,7 @@ package JavaBasic;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
@@ -31,7 +32,7 @@ public class Topic_08_For_Exercise {
 		}
 	}
 	
-	@Test
+	//@Test
 	public void TC_03_Sum_of_Even_Number () {
 		List<Integer> evenNumber = new ArrayList<Integer>();
 		for (int i = 0; i <= 10; i++) {
@@ -43,4 +44,66 @@ public class Topic_08_For_Exercise {
 				
 		System.out.println(sum);		
 	}
+	
+	//@Test
+	public void TC_04_Sum_of_All_Numbers () {
+		int numberC = scanner.nextInt();
+		int numberD = scanner.nextInt();
+		
+		List<Integer> sumAllNumbers = new ArrayList<Integer>();
+		for (int i = numberC; i <= numberD; i++) {
+				sumAllNumbers.add(i);
+		}
+		int sum = sumAllNumbers.stream().mapToInt(Integer::intValue).sum();
+				
+		System.out.println(sum);		
+	}
+	
+	//@Test
+	public void TC_05_Sum_of_Odd_Numbers () {
+		int numberE = scanner.nextInt();
+		
+		List<Integer> sumOddNumbers = new ArrayList<Integer>();
+		for (int i = 0; i <= numberE; i++) {
+			if (i%2 != 0) {
+				sumOddNumbers.add(i);
+			}
+		}
+		int sum = sumOddNumbers.stream().mapToInt(Integer::intValue).sum();
+		System.out.println(sum);		
+
+	}
+	
+
+	//@Test
+	public void TC_06_List_Of_Numbers_Devided_Three () {
+		int numberF = scanner.nextInt();
+		int numberG = scanner.nextInt();
+		
+		List<Integer> devidedToThree = new ArrayList<Integer>();
+		for (int i = numberF; i <= numberG; i++) {
+			if (i%3 == 0) {
+				devidedToThree.add(i);
+			}
+		}
+		System.out.println(devidedToThree);		
+
+	}
+	
+	@Test
+	public void TC_7_Multiplication () {
+		int numberH = scanner.nextInt();
+		long factorial = 1;
+		for (int i = 2; i <= numberH; i++) {
+			factorial *= i;
+			
+			} 
+
+		
+		System.out.println(factorial);		
+	}
+	
+	
+
+	
 }
